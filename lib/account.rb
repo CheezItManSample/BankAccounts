7e952705bae5d143da4dd93d275361e3d1d7a0c0
Chris <chris@adadevelopersacademy.org>
2017-02-22 23:59:02 -0800

    Wave 1 completed.

--- lib/account.rb
+++ lib/account.rb
@@ -1,37 +1,16 @@
+require 'csv'
+
 module Bank
 
   class Account
     attr_accessor :id, :balance, :open_date
 
-    def initialize(id, balance, open_date = nil)
+    def initialize(id, balance)
 
       raise ArgumentError.new "Balance must be positive or 0" unless balance >= 0
       @id = id
       @balance = balance
       @open_date = open_date
-    end
-
-    def self.all
-      accounts = []
-
-      CSV.read("support/accounts.csv").each do |line|
-        id = line[0].to_i
-        balance = line[1].to_i
-        open_date = line[2]
-        accounts << Account.new(id, balance, open_date)
-      end
-
-      accounts
-    end
-
-    def self.find(account_number)
-
-      CSV.read("support/accounts.csv").each do |line|
-        if line[0] == account_number.to_s
-            return Bank::Account.new(line[0].to_i, line[1].to_i, line[2])
-        end
-      end
-      raise ArgumentError.new "Account does not exist"
     end
 
     def withdraw(amount)
